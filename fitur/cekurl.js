ef76cda58a240ba671ec16d818a53320d3ea7c8c
asyraff <145573395+ditt12@users.noreply.github.com>
2024-11-09 16:34:15 +0700

    Update cekurl.js

--- fitur/cekurl.js
+++ fitur/cekurl.js
@@ -1,25 +1,22 @@
 const axios = require('axios');
-const xss = require('xss');  // Import module xss untuk memeriksa potensi XSS
-
+const xss = require('xss');  
 module.exports = (bot, chatId, url) => {
-  // Validasi URL menggunakan regex
+  
   const regex = /^(http|https):\/\/[^\s$.?#].[^\s]*$/;
   if (!regex.test(url)) {
     bot.sendMessage(chatId, 'URL tidak valid! Pastikan menggunakan format http:// atau https://');
     return;
   }
 
-  // Fungsi untuk cek URL dan keamanan dari XSS
   const checkUrl = async () => {
     try {
-      // Periksa apakah URL mengandung potensi XSS
-      const cleanUrl = xss(url); // Cek apakah URL aman
+      
+      const cleanUrl = xss(url); 
       if (cleanUrl !== url) {
         bot.sendMessage(chatId, `URL ini mungkin rentan terhadap serangan XSS: ${url}`);
         return;
       }
 
-      // Lakukan HTTP request ke URL untuk memverifikasi apakah URL aktif
       const response = await axios.get(url);
       bot.sendMessage(chatId, `URL valid: ${url} - Status: ${response.status}`);
     } catch (error) {
