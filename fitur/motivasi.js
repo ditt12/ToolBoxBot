45e0abf069d1410721e3720cca301ea88def8515
asyraff <145573395+ditt12@users.noreply.github.com>
2024-11-09 16:37:40 +0700

    Update motivasi.js

--- fitur/motivasi.js
+++ fitur/motivasi.js
@@ -1,15 +1,12 @@
-const axios = require('axios');  // Import axios untuk HTTP request
+const axios = require('axios');  
 
-// Fungsi untuk mendapatkan kutipan motivasi dari API
 async function sendMotivasi(bot, chatId) {
   try {
     const response = await axios.get('https://zenquotes.io/api/random');
     
-    // Ambil kutipan dari API response
     const quote = response.data[0].q;
     const author = response.data[0].a;
 
-    // Kirimkan kutipan ke chat Telegram
     bot.sendMessage(chatId, `"${quote}" - ${author}`);
   } catch (error) {
     console.error('Error fetching motivation:', error);
