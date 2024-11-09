457459bc7395da37ea246362a8a321722bf841c5
asyraff <145573395+ditt12@users.noreply.github.com>
2024-11-09 16:36:58 +0700

    Update joke.js

--- fitur/joke.js
+++ fitur/joke.js
@@ -1,16 +1,12 @@
 const axios = require('axios');
 
-// Fungsi untuk mengirimkan lelucon acak
 async function sendJoke(bot, chatId) {
   try {
-    // Mengambil data lelucon dari API
     const response = await axios.get('https://official-joke-api.appspot.com/jokes/random');
     const joke = response.data;
 
-    // Format lelucon
     const jokeText = `${joke.setup}\n\n${joke.punchline}`;
 
-    // Kirim lelucon ke pengguna
     bot.sendMessage(chatId, jokeText);
   } catch (error) {
     console.error('Error fetching joke:', error);
