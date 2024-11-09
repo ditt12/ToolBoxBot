bc025bbad1966b5fbf36b8e4f5cc60214b5cdb0a
asyraff <145573395+ditt12@users.noreply.github.com>
2024-11-09 16:36:06 +0700

    Update fototolink.js

--- fitur/fototolink.js
+++ fitur/fototolink.js
@@ -1,29 +1,23 @@
 const axios = require('axios');
 const FormData = require('form-data');
 
-// Fungsi untuk mengubah foto menjadi link menggunakan API ImgBB
 async function fotoToLink(bot, chatId, photo) {
-  const imgbbApiKey = '9de4a193e1dac4d9a112f35664aa07bd';  // Ganti dengan API Key ImgBB yang baru
+  const imgbbApiKey = '9de4a193e1dac4d9a112f35664aa07bd';  
 
   try {
-    // Mengambil foto dari pengguna
     const photoId = photo.file_id;
 
-    console.log('Photo ID:', photoId);  // Log untuk memeriksa ID foto
+    console.log('Photo ID:', photoId); 
 
-    // Mengunduh gambar dari ID
     const fileUrl = await bot.getFileLink(photoId);
-    console.log('File URL:', fileUrl);  // Log untuk memeriksa URL file
+    console.log('File URL:', fileUrl);  
 
-    // Mengambil data gambar
     const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
     console.log('Gambar berhasil diambil');  // Log untuk memeriksa apakah gambar berhasil diambil
 
-    // Membuat form data untuk upload ke ImgBB
     const form = new FormData();
     form.append('image', response.data, { filename: 'image.jpg' });
 
-    // Meng-upload gambar ke ImgBB menggunakan API
     const uploadResponse = await axios.post('https://api.imgbb.com/1/upload?key=' + imgbbApiKey, form, {
       headers: {
         ...form.getHeaders(),
@@ -34,7 +28,6 @@ async function fotoToLink(bot, chatId, photo) {
 
     const imageUrl = uploadResponse.data.data.url; // URL gambar yang di-upload ke ImgBB
 
-    // Kirim URL gambar menggunakan ImgBB
     bot.sendMessage(chatId, `Gambar berhasil diupload! \nLink Gambar: ${imageUrl}`);
   } catch (error) {
     console.error('Error:', error);  // Log kesalahan
