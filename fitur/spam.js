3bef20a2df1ff6e35bab264a17f7219e2fc745d8
asyraff <145573395+ditt12@users.noreply.github.com>
2024-11-09 16:39:37 +0700

    Update spam.js

--- fitur/spam.js
+++ fitur/spam.js
@@ -1,26 +1,21 @@
 const axios = require('axios');
-
 module.exports = (bot, chatId, url) => {
-  // Pastikan URL valid (http:// atau https://)
+  
   const regex = /^(http|https):\/\/[^\s$.?#].[^\s]*$/;
   if (!regex.test(url)) {
     bot.sendMessage(chatId, 'URL tidak valid! Pastikan menggunakan format http:// atau https://');
     return;
   }
 
-  // Fungsi untuk mengunjungi URL dan mengirimkan status
   const visitUrl = async () => {
     try {
-      // Lakukan HTTP request ke URL
       const response = await axios.get(url);
-      // Kirim status HTTP response ke chat
       bot.sendMessage(chatId, `Berhasil mengunjungi ${url} - Status: ${response.status}`);
     } catch (error) {
       bot.sendMessage(chatId, `Gagal mengunjungi ${url}. Error: ${error.message}`);
     }
   };
 
-  // Spam URL setiap detik
   const interval = setInterval(() => {
     visitUrl();
   }, 1000); // Setiap detik
